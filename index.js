186f811befb5962e5df65dd550c11185107e9604
Mike Castro <mkcastro24@gmail.com>
2020-10-03 14:59:58 +0800

    first

---
+++ index.js
@@ -0,0 +1,45 @@
+function calculateMonthly() {
+    const perHour = parseInt($("input[name=hours]").val());
+    const perDay = $("input[name=days]").val();
+    const perWeek = $("input[name=weeks]").val();
+    const perMonth = $("input[name=months]").val();
+    const currency = $("#currency").val();
+    let monthlySalary = 0;
+    const hoursPerDay = 8;
+    const daysPerWeek = 5;
+    const weeksPerMonth = 4;
+
+    if (perHour) {
+        monthlySalary = perHour * hoursPerDay * daysPerWeek * weeksPerMonth;
+    }
+
+    if (perDay) {
+        monthlySalary = perDay * daysPerWeek * weeksPerMonth;
+    }
+
+    if (perWeek) {
+        monthlySalary = perWeek * weeksPerMonth;
+    }
+
+    if (perMonth) {
+        monthlySalary = perMonth;
+    }
+
+    if (currency === "usd") {
+        monthlySalary = monthlySalary * 50;
+    }
+
+    appendToHistory(monthlySalary);
+}
+
+function appendToHistory(currency, salary) {
+    $(".history").append(`
+        <tr>
+            <td class="border px-4 py-2">${salary} php per month</td>
+        </tr>
+    `);
+}
+
+function dump(mixed) {
+    console.log(mixed);
+}
